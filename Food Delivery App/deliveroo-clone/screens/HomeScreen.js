5bef68386e9983a3c62cb4721a75795eed1d0b42
Khanh Le <khanh.le@longdan.co.uk>
2022-10-25 19:49:41 +0700

    start WhatsApp project, setup ChatListItem component

--- Food Delivery App/deliveroo-clone/screens/HomeScreen.js
+++ Food Delivery App/deliveroo-clone/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import SafeViewAndroid from '../components/SafeViewAndroid';
 import { useNavigation } from '@react-navigation/native';
@@ -20,9 +20,9 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView className="bg-white pt-5" style={SafeViewAndroid.AndroidSafeArea}>
-          <Text className="text-red-500">
+          <Text className="text-red-500"  style={{flex: 1, justifyContent: 'space-between'}}>
             {/* {Header} */}
-            <View className="flex flex-row pb-3 items-center mx-4 space-x-2">
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
               <Image
                 source={{
                   uri: "https://links.papareact.com/wru",
@@ -42,6 +42,17 @@ const HomeScreen = () => {
     
               <UserIcon size={35} color="#00CCBB" />
             </View>
+
+            {/* Search */}
+            <View className="flex-row space-x-2 items-center pb-2 mx-4">
+              <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                <SearchIcon color="gray" size={20} />
+                <TextInput placeholder='Restaurants and cuisines'
+                keyboardType='default'/>
+              </View>
+              {/* <AdjustmentsIcon color="#00CCBB" /> */}
+            </View>
+
           </Text>
         </SafeAreaView>
       );
---
+++ WhatsApp/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import ChatListItem from './src/components/ChatListItem';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ChatListItem />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
