64dc5dfbeaa0bcfefe40b23e4451ca9495f68148
Khanh Le <masterbruculous@gmail.com>
2022-10-26 11:24:16 +0700

    Update message for chatscreen

--- WhatsApp/src/screens/ChatScreen.js
+++ WhatsApp/src/screens/ChatScreen.js
@@ -1,28 +1,42 @@
-import { View, Text, ImageBackground, StyleSheet, FlatList } from 'react-native'
-import bg from '../../assets/images/BG.png'
-import messages from '../../assets/data/messages.json'
-import Message from '../components/Message'
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import bg from "../../assets/images/BG.png";
+import messages from "../../assets/data/messages.json";
+import Message from "../components/Message";
+import InputBox from "../components/InputBox";
 
 const ChatScreen = () => {
   return (
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList 
-        data={messages}
-        renderItem={({ item }) => <Message message={item} />}
-        inverted
-      />
-    </ImageBackground>
-  )
-}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
 
 const styles = StyleSheet.create({
-    bg: {
-        flex: 1,
-    },
-    list: {
-        padding: 10,
-
-    }
+  bg: {
+    flex: 1,
+  },
+  list: {
+    padding: 10,
+  },
 });
 
-export default ChatScreen
+export default ChatScreen;
